4669b435ffab4ccd5150d39b89b8cf750f547c21
ntt-vulk <vu.lk@nttnetworks.com>
2021-05-27 10:57:51 +0700

    fix(index): revert code for index file

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-// const express = require("express");
+const express = require("express");
 const rateLimit = require("express-rate-limit");
-// //https://www.npmjs.com/package/express-rate-limit
-// const app = express();
+//https://www.npmjs.com/package/express-rate-limit
+const app = express();
 const port = 3000;
-const host = 'localhost';
 
-// const limiter = rateLimit({
-// 	windowMs: 15 * 60 * 1000, // 15 minutes
-// 	max: 10, // mean: maximun 100 request per 15 minutes
-// });
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // mean: maximun 100 request per 15 minutes
+});
 
-// // Apply to all requests
-// app.use(limiter);
+// Apply to all requests
+app.use(limiter);
 
-// app.get('/', limiter, (req, res) => res.send('Hello world'));
+app.get('/', limiter, (req, res) => res.send('Hello world'));
 
 const createAccountLimiter = rateLimit({
 	windowMs: 60 * 60 * 1000, // 1 hours window
@@ -21,21 +20,8 @@ const createAccountLimiter = rateLimit({
 	message: {'status': 429, 'message': "Too many requests"},
 });
 
-// app.get("/create-account", createAccountLimiter, (req, res) => {
-// 	res.send("create account");
-// })
+app.get("/create-account", createAccountLimiter, (req, res) => {
+	res.send("create account");
+})
 
-// app.listen(port, () => console.log(`Example app listening on port ${port}`));
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
-
-app.use(createAccountLimiter);
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
-});
-
-server.listen(3000, () => {
-  console.log('listening on *:3000');
-});
+app.listen(port, () => console.log(`Example app listening on port ${port}`));
